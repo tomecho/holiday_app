260611e335a1ea5dcf75b16f53dd8abb7f31a2ee
Tom Peck <tapeck@umass.edu>
2015-12-09 16:32:24 -0500

    finished

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,9 +1,6 @@
 class Country < ActiveRecord::Base
+  before_save { self.alpha2.upcase! }
   has_many :holidays
   validates :name, presence: true
   validates :alpha2, length: { is: 2 }, presence: true, uniqueness: true
-#  before_create :alpha2_upcase
-#  def alpha2_upcase
-#    self.alpha2.upcase!
-#  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151202203939) do
+ActiveRecord::Schema.define(version: 20151209211932) do
 
   create_table "countries", force: :cascade do |t|
     t.string "name"
@@ -25,5 +25,8 @@ ActiveRecord::Schema.define(version: 20151202203939) do
     t.datetime "updated_at", null: false
     t.integer  "country_id"
   end
+
+  add_index "holidays", ["date"], name: "index_holidays_on_date"
+  add_index "holidays", ["name"], name: "index_holidays_on_name"
 
 end
